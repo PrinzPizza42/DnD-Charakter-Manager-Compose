c5fa95f479f82a4b88d215f00cda72338b18d1a6
Luca Weimer <125275369+PrinzPizza42@users.noreply.github.com>
2026-01-16 22:17:41 +0100

    Refactor tabElements to one methode

--- src/main/kotlin/main/TabSelector.kt
+++ src/main/kotlin/main/TabSelector.kt
@@ -15,14 +15,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.paddingFromBaseline
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.onClick
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -32,18 +28,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.lerp
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.toPainter
-import androidx.compose.ui.input.pointer.PointerEvent
-import androidx.compose.ui.input.pointer.onPointerEvent
 import androidx.compose.ui.input.pointer.pointerMoveFilter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 object TabSelector {
@@ -88,7 +80,6 @@ object TabSelector {
                         .onClick(enabled = true, onClick = {
                             if(selectedInventory.value != null) {
                                 Write.safe(selectedInventory.value!!)
-                                println("Auto saved inv " + selectedInventory.value!!.name + " on close")
                                 selectedInventory.value = null
                                 Read.readData()
                             }
@@ -123,111 +114,77 @@ object TabSelector {
                     .background(lerp(Color.LightGray, Color.DarkGray, 0.6f), RoundedCornerShape(5.dp))
             ) {
                 //Show inv button
-                var selectedInv by remember(showInventory.value) { mutableStateOf(showInventory.value) }
-                var hoveredOverInv by remember { mutableStateOf(false) }
-
-                val backgroundColorInv by animateColorAsState(
-                    if(selectedInv) lerp(Color.LightGray, Color.DarkGray, 0.2f) else lerp(Color.LightGray, Color.DarkGray, 0.6f),
-                    animationSpec = tween (300)
+                tabElement(
+                    {},
+                    showInventory,
+                    ImageLoader.loadImageFromResources("backPackIcon.png").get().toPainter()
                 )
-
-                val shadowInv by animateFloatAsState(
-                    if(hoveredOverInv) 10f else 1f,
-                    animationSpec = tween (300)
-                )
-
-                val scaleInv by animateFloatAsState(
-                    if(hoveredOverInv) 1.1f else 1f,
-                    animationSpec = tween (300)
-                )
-
-                Box(
-                    Modifier
-                        .graphicsLayer(scaleX = scaleInv, scaleY = scaleInv)
-                        .fillMaxWidth()
-                        .padding(0.dp, 10.dp, 0.dp, 0.dp)
-                        .clickable(enabled = true, onClick = {
-                            showInventory.value = !showInventory.value
-                            if(showInventory.value) showInvAnimationEndInv.value = showInventory.value
-                            println("showInv: " + showInventory.value)
-                        })
-                        .shadow(shadowInv.dp)
-                        .background(backgroundColorInv, RoundedCornerShape(5.dp))
-                        .pointerMoveFilter(
-                            onEnter = {
-                                hoveredOverInv = true
-                                false
-                            },
-                            onExit = {
-                                hoveredOverInv = false
-                                false
-                            }
-                        )
-                ) {
-                    val backPackIcon = remember { ImageLoader.loadImageFromResources("backPackIcon.png").get().toPainter() }
-                    Image(
-                        painter = backPackIcon,
-                        contentScale = ContentScale.FillWidth,
-                        contentDescription = "inv",
-                        modifier = Modifier
-                            .padding(2.dp)
-                            .align(Alignment.Center)
-                    )
-                }
 
                 //Show scrollPanel button
-                var selectedSpells by remember(showScrollPanel.value) { mutableStateOf(showScrollPanel.value) }
-                var hoveredOverSpells by remember { mutableStateOf(false) }
-
-                val backgroundColorSpells by animateColorAsState(
-                    if(selectedSpells) lerp(Color.LightGray, Color.DarkGray, 0.2f) else lerp(Color.LightGray, Color.DarkGray, 0.6f),
-                    animationSpec = tween (300)
+                tabElement(
+                    {},
+                    showScrollPanel,
+                    ImageLoader.loadImageFromResources("scrollIcon.png").get().toPainter()
                 )
-
-                val shadowSpells by animateFloatAsState(
-                    if(hoveredOverSpells) 10f else 1f,
-                    animationSpec = tween (300)
-                )
-
-                val scaleSpells by animateFloatAsState(
-                    if(hoveredOverSpells) 1.1f else 1f,
-                    animationSpec = tween (300)
-                )
-
-                Box(
-                    Modifier
-                        .graphicsLayer(scaleX = scaleSpells, scaleY = scaleSpells)
-                        .fillMaxWidth()
-                        .padding(0.dp, 10.dp, 0.dp, 10.dp)
-                        .clickable(enabled = true, onClick = {
-                            showScrollPanel.value = !showScrollPanel.value
-                            if(showScrollPanel.value) showInvAnimationEndSpells.value = showScrollPanel.value
-                        })
-                        .shadow(shadowSpells.dp)
-                        .background(backgroundColorSpells, RoundedCornerShape(5.dp))
-                        .pointerMoveFilter(
-                            onEnter = {
-                                hoveredOverSpells = true
-                                false
-                            },
-                            onExit = {
-                                hoveredOverSpells = false
-                                false
-                            }
-                        )
-                ) {
-                    val scrollIcon = remember { ImageLoader.loadImageFromResources("scrollIcon.png").get().toPainter() }
-                    Image(
-                        painter = scrollIcon,
-                        contentScale = ContentScale.FillWidth,
-                        contentDescription = "inv",
-                        modifier = Modifier
-                            .padding(2.dp)
-                            .align(Alignment.Center)
-                    )
-                }
             }
+        }
+    }
 
+    @OptIn(ExperimentalComposeUiApi::class)
+    @Composable
+    fun tabElement(
+        onClick: () -> Unit,
+        showPanel: MutableState<Boolean>,
+        icon: Painter
+    ) {
+        var hoveredOverSpells by remember { mutableStateOf(false) }
+
+        val backgroundColor by animateColorAsState(
+            if(showPanel.value) lerp(Color.LightGray, Color.DarkGray, 0.2f) else lerp(Color.LightGray, Color.DarkGray, 0.6f),
+            animationSpec = tween (300)
+        )
+
+        val shadow by animateFloatAsState(
+            if(hoveredOverSpells) 10f else 1f,
+            animationSpec = tween (300)
+        )
+
+        val scale by animateFloatAsState(
+            if(hoveredOverSpells) 1.1f else 1f,
+            animationSpec = tween (300)
+        )
+
+        Box(
+            Modifier
+                .graphicsLayer(scaleX = scale, scaleY = scale)
+                .fillMaxWidth()
+                .padding(0.dp, 10.dp, 0.dp, 10.dp)
+                .clickable(enabled = true, onClick = {
+                    showPanel.value = !showPanel.value
+                    onClick()
+//                    if(showPanel.value) showInvAnimationEndSpells.value = showPanel.value
+                })
+                .shadow(shadow.dp)
+                .background(backgroundColor, RoundedCornerShape(5.dp))
+                .pointerMoveFilter(
+                    onEnter = {
+                        hoveredOverSpells = true
+                        false
+                    },
+                    onExit = {
+                        hoveredOverSpells = false
+                        false
+                    }
+                )
+        ) {
+            Image(
+                painter = icon,
+                contentScale = ContentScale.FillWidth,
+                contentDescription = "icon",
+                modifier = Modifier
+                    .padding(2.dp)
+                    .align(Alignment.Center)
+            )
         }
     }
 }
